914315e0fd9c81848e2cc26c3ef19c8f6a9f8c64
Albert Dayn <Albert.Dayn@Colorado.edu>
2020-06-17 01:32:07 -0600

    Fixed arrows

--- resume-site/src/tabs/Home.js
+++ resume-site/src/tabs/Home.js
@@ -38,103 +38,40 @@ class Home extends React.Component {
 			</div>
 			<div className="hobbies-area">
 				<div className="content">
-					<div className="hobby-row">
-						<div className="hobby-cell-l">
+					<div className="hobby-grid">
+						<div className="bologna">
 							<img className="hobby-media" src={bologna}
 							     alt={"Bologna"}/>
+							<div className="arrow-1 arrow"/>
 						</div>
-						<div className="hobby-cell-r center-text text-1">This is
-							my dog Balogna!
+						<div className="bologna hobby-text center-text">
+							This is Balogna, doing what she does best:
+							laying down. Also happens to be my favorite
+							activity.
 						</div>
-						<div className="arrow-1 arrow"></div>
-					</div>
-					<div className="hobby-row">
-						<div className="hobby-cell-l center-text text-2">Shramps
-							had babies!
+
+
+						<div className="shramp hobby-text center-text">
+							I grow plants and keep aquariums, and my 4
+							shrimp moms all decided to have babies at the
+							same time!
 						</div>
-						<div className="arrow-2 arrow"></div>
-						<div className="hobby-cell-r">
+						<div className="shramp">
 							<video className="hobby-media" src={shramps}
 							       autoPlay loop muted/>
+							<div className="arrow-2 arrow"/>
 						</div>
-					</div>
-					<div className="hobby-row">
-						<div className="hobby-cell-l ">
+
+						<div className="eclair">
 							<img className="hobby-media" src={eclairs}
 							     alt={"Eclairs"}/>
+							<div className="arrow-1 arrow"/>
 						</div>
-						<div className="hobby-cell-r center-text text-3">This is
-							my dog Balogna!
+						<div className="eclair hobby-text center-text">
+							Only thing more more frustrating than programming
+							is cooking, but C++ never tasted as good as
+							eclairs!
 						</div>
-						<div className="arrow-3 arrow"></div>
-					</div>
-				</div>
-				<div className="hobbies-area">
-					<div className="content">
-						<div className="hobby-grid">
-							<div className="bologna">
-								<img className="hobby-media" src={bologna}
-								     alt={"Bologna"}/>
-								{/*<div className="arrow-up arrow"> </div>*/}
-							</div>
-							<div className="bologna hobby-text center-text">
-								This is Balogna, doing what she does best:
-								laying down. Also happens to be my favorite
-								activity.
-							</div>
-
-
-							<div className="shramp hobby-text center-text">
-								I grow plants and keep aquariums, and my 4
-								shrimp moms all decided to have babies at the
-								same time!
-							</div>
-							<div className="shramp">
-								<video className="hobby-media" src={shramps}
-								       autoPlay loop muted/>
-							</div>
-
-							{/* Row 2 */}
-							<div className="eclair">
-								<img className="hobby-media" src={eclairs}
-								     alt={"Eclairs"}/>
-							</div>
-							<div className="eclair hobby-text center-text">
-								The only thing more more frustrating, soul
-								crushing and infuriating than programming
-								is cooking, but C++ never tasted as good as th
-							</div>
-						</div>
-					</div>
-					<div className="hobby-row">
-						<div className="hobby-cell-l">
-							<img className="hobby-media" src={bologna}
-							     alt={"Bologna"}/>
-						</div>
-						<div className="hobby-cell-r center-text text-1">This is
-							my dog Balogna!
-						</div>
-						<div className="arrow-1 arrow"></div>
-					</div>
-					<div className="hobby-row">
-						<div className="hobby-cell-l center-text text-2">Shramps
-							had babies!
-						</div>
-						<div className="arrow-2 arrow"></div>
-						<div className="hobby-cell-r">
-							<video className="hobby-media" src={shramps}
-							       autoPlay loop muted/>
-						</div>
-					</div>
-					<div className="hobby-row">
-						<div className="hobby-cell-l ">
-							<img className="hobby-media" src={eclairs}
-							     alt={"Eclairs"}/>
-						</div>
-						<div className="hobby-cell-r center-text text-3">This is
-							my dog Balogna!
-						</div>
-						<div className="arrow-3 arrow"></div>
 					</div>
 				</div>
 			</div>
